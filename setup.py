8728c0cedd29e55812d789f2e8c7e4ae7cba5129
Derek Dahmer <derekdahmer@gmail.com>
2011-12-29 19:21:05 -0500

    PIP packaging files

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='ticketevolution',
+      version='0.1dev',
+      packages=['ticketevolution',],
+      url='https://github.com/ticketevolution/ticketevolution-python',
+      author='Derek Dahmer',
+      author_email='derekdahmer@gmail.com',
+      )
