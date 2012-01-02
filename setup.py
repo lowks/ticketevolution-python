d5b58f6fdee7ebe59d48048fb014c8572919877e
Derek Dahmer <derekdahmer@gmail.com>
2012-01-02 18:04:13 -0500

    Bumping version to 0.1

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup
 setup(name='ticketevolution',
-      version='0.1dev',
+      version='0.1',
       packages=['ticketevolution',],
       url='https://github.com/ticketevolution/ticketevolution-python',
       author='Derek Dahmer',
       author_email='derekdahmer@gmail.com',
-      )
+)
+
